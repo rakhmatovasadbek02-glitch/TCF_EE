6ec676f72a21b7d3ab098c488232deea6f27c9ab
rakhmatovasadbek02-glitch <rakhmatovasadbek02@icloud.com>
2026-04-22 01:51:53 +0500

    upd

--- script.js
+++ script.js
@@ -34,3 +34,32 @@ async function saveWriting() {
 
   alert("Saved!");
 }
+function startTimer() {
+  document.querySelector("button").disabled = true;
+  document.getElementById("writingArea").disabled = false;
+
+  timer = setInterval(() => {
+    timeLeft--;
+
+    let minutes = Math.floor(timeLeft / 60);
+    let seconds = timeLeft % 60;
+
+    document.getElementById("timer").textContent =
+      `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+
+    if (timeLeft <= 0) {
+      clearInterval(timer);
+      document.getElementById("writingArea").disabled = true;
+      alert("Time is up!");
+    }
+  }, 1000);
+}
+document.getElementById("writingArea").focus();
+
+const textarea = document.getElementById("writingArea");
+
+textarea.addEventListener("input", () => {
+  const words = textarea.value.trim().split(/\s+/).filter(Boolean);
+  document.getElementById("wordCount").textContent =
+    `${words.length} words`;
+});
